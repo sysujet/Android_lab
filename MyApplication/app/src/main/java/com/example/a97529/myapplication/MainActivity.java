8c51725bbb1abd480a7022dc9672ae9136b46447
jet <32641394+sysujet@users.noreply.github.com>
2017-10-09 22:08:27 +0800

    homework1
    homework1

---
+++ MyApplication/app/src/main/java/com/example/a97529/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.a97529.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "fuck you", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
